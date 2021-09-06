524d892ea167451b6adaac5d002bd0cb5db2b554
Justin Xia <justinxia9@gmail.com>
2021-09-06 11:30:13 -0400

    Add end time display

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000); // to get seconds
@@ -24,4 +26,11 @@ function displayTimeLeft(seconds) {
   const display = `${minutes}:${remainderSeconds < 10 ? "0" : ""}${remainderSeconds}`; // adds 0 in front of single digit seconds
   document.title = display;
   timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${hour > 12 ? hour - 12: hour}:${minutes < 10 ? "0" : ""}${minutes}`; // convert from 24h to 12h
 }
